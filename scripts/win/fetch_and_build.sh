cdf2fa61ea458525479b3a53d028c229261a30ad
Dan Balasescu <smoogipoo@smgi.me>
2022-04-27 22:04:28 +0900

    Adjust job names

--- scripts/win/fetch_and_build.sh
+++ scripts/win/fetch_and_build.sh
@@ -4,17 +4,17 @@ cross_arch=''
 cross_prefix=''
 
 case $arch in
-    win-x86)
+    x86)
         cross_arch='x86'
         cross_prefix='i686-w64-mingw32-'
         ;;
 
-    win-x64)
+    x64)
         cross_arch='x86'
         cross_prefix='x86_64-w64-mingw32-'
         ;;
 
-    win-arm64)
+    arm64)
         cross_arch='aarch64'
         cross_prefix='aarch64-w64-mingw32-'
         ;;
@@ -39,4 +39,4 @@ make -j$(nproc)
 make install
 
 mkdir -p ../build-$arch
-cp build-$arch/bin/*.dll ../build-$arch/
+cp build-$arch/bin/*.dll ../build-win-$arch/
